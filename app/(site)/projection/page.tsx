2a1ba950584dce7bed016ac45ec9d0c7639230f7
Jeel darji <darjijeel31@gmail.com>
2026-08-22 10:09:52 +0530

    Push project code to GitHub

--- app/(site)/projection/page.tsx
+++ app/(site)/projection/page.tsx
@@ -16,7 +16,7 @@ const PROJECTS_DATA = [
     title: "Story of Ramayan",
     slug: "Ramayan",
     image: "/Ramayan1.png",
-    videoUrl: "https://iframe.mediadelivery.net/embed/583855/7838c69b-7d21-40e4-af72-ec682884baa4?autoplay=true&loop=false&muted=false&preload=true&responsive=true",
+    videoUrl: "https://iframe.mediadelivery.net/play/583855/912f0d66-3718-4682-ab0d-ec228ae1af66",
     category: "Ramayan",
   },
 {
